e52ae361e89649caaa23de281d97896c34ce0cfb
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-24 17:38:16 -0500

    added blog theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-mdx`],
+  plugins: [
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/blog`,
+        mdxOtherwiseConfigured: true,
+      },
+    },
+  ],
 };
