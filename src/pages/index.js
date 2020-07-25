1fbabab793915d9ad5584d65df73d832b7c37cc2
Khris Punzalan <khrispunzalan@gmail.com>
2020-07-24 19:00:30 -0500

    added image using graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-// import Img from 'gatsby-image';
+import Img from 'gatsby-image';
+import { graphql } from 'gatsby';
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div>
       <h1>Hello</h1>
-      {/* <Img fluid={}/> */}
+      <Img fluid={data.codelandImage.childImageSharp.fluid} />
     </div>
   );
 }
+
+export const query = graphql`
+  query ImageQuery {
+    codelandImage: file(relativePath: { eq: "download.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
